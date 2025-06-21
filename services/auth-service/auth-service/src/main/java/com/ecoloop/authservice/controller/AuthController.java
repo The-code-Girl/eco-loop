d1e3e1c4dcbf0b0deb8712a06b567f60137749e4
The-code-Girl <anjalijoshi710@gmail.com>
2025-06-21 19:27:22 +0530

    Auth-Service : controller

--- services/auth-service/auth-service/src/main/java/com/ecoloop/authservice/controller/AuthController.java
+++ services/auth-service/auth-service/src/main/java/com/ecoloop/authservice/controller/AuthController.java
@@ -1,5 +1,36 @@
 package com.ecoloop.authservice.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecoloop.authservice.dto.JwtResponse;
+import com.ecoloop.authservice.dto.LoginRequest;
+import com.ecoloop.authservice.dto.RegisterRequest;
+import com.ecoloop.authservice.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    private final AuthService authService;
+    public AuthController(AuthService authService){
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest){
+        String response = authService.register(registerRequest);
+        return ResponseEntity.ok(response);
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody LoginRequest loginRequest){
+        JwtResponse response = authService.login(loginRequest);
+        return ResponseEntity.ok(response);
+    }
     
 }
